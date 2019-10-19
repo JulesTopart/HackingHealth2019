4251083d47c99a513dbea4b7154390356188bc81
Jules Topart <jules.topart@gmail.com>
2019-10-19 10:58:36 +0200

    fix

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -1,10 +1,52 @@
+# coding: utf-8
 import nltk
-import matplotlib
+import os
+import csv
+
+from bs4 import BeautifulSoup
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, '../data/data_janvier_fevrier_2017.html')
+
+
+fname =  os.path.join(dirname, '../data/out.csv')
+file = open(fname, "wb")
+
+try:
+    writer = csv.writer(file, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(["Mots", "Occurences"])
+    #for key,val in freq.items():
+    #    writer.writerow((str(key),val))
+
+finally:
+    file.close()
+
+
+
+
+
+
+dataRaw = open(filename, encoding="utf8")
+
+soup = BeautifulSoup(dataRaw, 'html.parser')
+
+dataArray = soup.findAll('tr')
+reportArray = []
+
+for k in dataArray:
+    temp = k.find_all("td")
+    if len(temp) >= 7:
+        reportArray.append(temp[6].string)
+
 tokenizer = nltk.RegexpTokenizer(r'\w+')
-tokens = tokenizer.tokenize("Le conjonctif sous-jacent est fibreux, sans aucune infiltration suspecte. ETUDE IMMUNOHISTOCHIMIQUE SUR COUPE EN PARAFFINE (Machine automate Roche Ventana Benchmark XT-ULTRA) : CD3 (Polyclonal, Dako, 1/200)  : marquage diffus. CD20 (Clone L26, BIO SB, 1/1000) : rares éléments de taille moyenne ( marquage membranaire ). Kappa (POLY, Dako, 1/15 000), Lambda (POLY, Dako, 1/15 000) : marquages de densité comparable, signant la polyclonalité. CD5 (Clone 4C7, Thermo Scientific, 1/150) : négatif. Cet aspect associé au contexte morphologique confirme la bénignité de cette hypertrophie lymphoïde. CONCLUSION POLE SUPERIEUR AMYGDALE GAUCHE (biopsie) : Hyperplasie lymphoïde amygdalienne diffuse, au profil immunologique de lésion réactionnelle. Métaplasie malpighienne nodulaire au niveau de cryptes anciennes. Pas de malignité. ")
+
+reports = ""
+for report in reportArray:
+    reports += report
+
+tokens = tokenizer.tokenize(reports)
 freq = nltk.FreqDist(tokens)
  
-for key,val in freq.items():
-    print (str(key) + ':' + str(val))
-    
-freq.plot(20, cumulative=False)
+
+
+
